17d1d342ff34294ebdafbacdbffc53d5fe7e3033
MrJen <cmcc@mrjen.local>
2017-10-17 20:57:07 +0800

    广场和发布页面

---
+++ wafer2-startup-master/client/pages/market/market.js
@@ -0,0 +1,46 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+  logos: [{
+    image: "/image/logo1.png",
+    title: "呐喊求助"
+  }, {
+    image: "/image/logo2.png",
+    title: "拼车组团"
+  }, {
+    image: "/image/logo3.png",
+    title: "业主投票"
+  }, {
+    image: "/image/logo4.png",
+    title: "失物招领"
+  }, {
+    image: "/image/logo5.png",
+    title: "广场开店"
+  }, {
+    image: "/image/logo6.png",
+    title: "二手交易"
+  }, {
+    image: "/image/logo7.png",
+    title: "到家服务"
+  }, {
+    image: "/image/logo8.png",
+    title: "业主租赁"
+  }, {
+    image: "/image/logo9.png",
+    title: "宠物乐园"
+  }, {
+    image: "/image/logo10.png",
+    title: "约会活动"
+  }
+  ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 2000,
+    duration: 1000
+  }
+  
+})
